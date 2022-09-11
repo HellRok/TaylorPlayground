e0c1ed60b859a2e43edca6e60435093137f94c1b
Sean Earle <sean.r.earle@gmail.com>
2022-09-11 15:43:02 +0930

    Add the gamepad example

--- build.rb
+++ build.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/env ruby
 
 def generate_example_selector
-  examples = Dir.entries('./examples').reject! { _1 == '.' || _1 == '..' }
-  select_element = '<select id="example">';
+  examples = Dir.entries('./examples').reject! { _1 == '.' || _1 == '..' }.sort
+  option_elements = '';
   example_elements = '';
 
   examples.each { |example|
     code = File.read(File.join('examples', example))
-    select_element += "<option value='##{example.gsub('.rb', '')}'>#{example}</option>"
+    option_elements += "<option value='##{example.gsub('.rb', '')}'>#{example}</option>"
     example_elements += "<div id='#{example.gsub('.rb', '')}' class='hidden'>#{code}</div>"
   }
 
   <<-EOS
-    #{select_element}</select>
+    <select id="example">#{option_elements}</select>
     #{example_elements}
   EOS
 end
