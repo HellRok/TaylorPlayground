87c3d6cf835fd57c161d09d91d56b0c483d58d55
Sean Earle <sean.r.earle@gmail.com>
2025-09-19 11:16:08 +0930

    Add documentation link

--- app/javascripts/lib/toolbar.test.tsx
+++ app/javascripts/lib/toolbar.test.tsx
@@ -1,49 +1,52 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Toolbar } from "./toolbar";
+import { StateProvider } from "./state";
 
 describe("Toolbar", () => {
-  let theVersion;
-  let theCode;
-  const defaultProps = {
-    version: "v0.4.0",
-    setVersion: (value: string) => { theVersion = value },
-    setCode: (value: string) => { theCode = value },
-  };
-
   xit("renders the run button", () => {
-    render(<Toolbar {...defaultProps} />);
+    render(<Toolbar />);
 
     expect(screen.getByTestId("run")).toBeDefined();
   });
 
   xit("renders the share button", () => {
-    render(<Toolbar {...defaultProps} />);
+    render(<Toolbar />);
 
     expect(screen.getByTestId("share")).toBeDefined();
   });
 
   it("renders the version selector", () => {
-    render(<Toolbar {...defaultProps} />);
+    render(<Toolbar />);
 
     expect(screen.getByTestId("version")).toBeDefined();
   });
 
   it("renders the example selector", () => {
-    render(<Toolbar {...defaultProps} />);
+    render(<Toolbar />);
 
     expect(screen.getByTestId("example")).toBeDefined();
   });
 
-  xit("links to the documentation", () => {
-    render(<Toolbar {...defaultProps} />);
+  it("links to the documentation for the selected version", async () => {
+    const user = userEvent.setup();
+    render(
+      <StateProvider>
+        <Toolbar />
+      </StateProvider>
+    );
 
-    expect(screen.getByTestId("documentation")).toBeDefined();
+    const docLink: HTMLAnchorElement = screen.getByTestId("documentation");
+    expect(docLink.href).toEqual("https://taylormadetech.dev/documentation/taylor/v0.4.0/");
+
+    await user.selectOptions(screen.getByTestId("version"), "v0.3.14.1");
+    expect(docLink.href).toEqual("https://taylormadetech.dev/documentation/taylor/v0.3.14.1/");
   });
 
   it("links to the Taylor website", () => {
-    render(<Toolbar {...defaultProps} />);
+    render(<Toolbar />);
 
     const taylorLink: HTMLAnchorElement = screen.getByTestId("taylor");
     expect(taylorLink.href).toEqual("https://taylormadetech.dev/");
--- app/javascripts/lib/toolbar.tsx
+++ app/javascripts/lib/toolbar.tsx
@@ -1,7 +1,10 @@
+import { useStateContext } from "./state";
 import { Version } from "./version";
 import { Example } from "./example";
 
 export function Toolbar() {
+  const [state, _] = useStateContext();
+
   return (
     <div className="toolbar" data-testid="toolbar">
       <Version />
@@ -13,6 +16,14 @@ export function Toolbar() {
         data-testid="taylor"
       >
         Home
+      </a>
+      <a
+        className="button"
+        href={`https://taylormadetech.dev/documentation/taylor/${state.version}/`}
+        target="_blank"
+        data-testid="documentation"
+      >
+        Documentation
       </a>
     </div>
   );
