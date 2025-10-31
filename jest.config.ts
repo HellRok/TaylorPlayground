776730e7827b1ffab66e6a147206046d8d886193
Sean Earle <sean@fresho.com>
2025-10-31 10:49:39 +1030

    Remove redundant config

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,3 @@
-// export default {
-//   testEnvironment: "jsdom",
-//   transform: {
-//     "^.+\\.(js|jsx)$": [
-//       "esbuild-jest",
-//       {
-//         jsxFactory: "m",
-//         jsxFragment: '"["',
-//       },
-//     ]
-//   },
-// };
 const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
@@ -19,6 +7,5 @@ module.exports = {
   testEnvironment: "jsdom",
   transform: {
     ...tsJestTransformCfg,
-    "^.+\\.(js|jsx)$": ["esbuild-jest"],
   },
 };
