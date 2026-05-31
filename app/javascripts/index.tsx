2ace2975bf1ee94f7a17e6385473b4991c2924f4
Sean Earle <sean.r.earle@gmail.com>
2026-05-31 13:35:15 +0930

    Remove a bunch of old code

--- app/javascripts/index.tsx
+++ app/javascripts/index.tsx
@@ -1,60 +1,9 @@
 import { Playground } from "./lib/playground";
 
-// import { Toolbar } from "./lib/toolbar";
-// import { Code } from "./lib/code";
-// import { Editor } from "./lib/editor";
-// import { Example } from "./lib/example";
-// import { Version } from "./lib/version";
-
 import { createRoot } from "react-dom/client";
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector("#playground");
   const root = createRoot(container!);
   root.render(<Playground />);
-
-  // Editor.setup();
-
-  // document.querySelector("#link").addEventListener("click", () => {
-  //   generateLink();
-  // });
-
-  // const exampleSelector = document.querySelector("#example");
-  // exampleSelector.addEventListener("change", () => {
-  //   loadExample();
-  //   runCode();
-  // });
-
-  // if (getCodeFromURL()) {
-  //   exampleSelector.value = "#custom";
-  //   document.querySelector(exampleSelector.value).textContent =
-  //     getCodeFromURL();
-  //   setCode(getCodeFromURL());
-  // } else {
-  //   exampleSelector.value = "#welcome";
-  //   loadExample();
-  // }
-  // runCode();
-  // Toolbar.setup();
-
-  // Code.setup();
-  // Example.setup();
-  // Version.setup();
-  // Version.load();
-
-  // let code = Code.fromAnchor();
-  // console.log(code);
-
-  // Editor.setCode(Example.code());
-  // Code.run();
 });
-
-// function generateLink() {
-//   const code = window.editor.state.doc.toString();
-//   window.location.hash = `#${LZString.compressToEncodedURIComponent(code)}`;
-// }
-//
-// function loadExample() {
-//   const select = document.querySelector("#example");
-//   setCode(document.querySelector(select.value).textContent);
-// }
